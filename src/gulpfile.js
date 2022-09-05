6b3f900f7f47873623c534d6b707f755b032c8e4
Yotam Faraggi <yotam@curalife.com>
2022-09-05 14:47:06 +0300

    Mon Sep  5 14:47:06 IDT 2022

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -1,10 +1,30 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 
+// Asset paths
+const srcSCSS = `scss/**/*.scss`;
+const srcJS = `js/*.js`;
+const assetsDir = `../assets/`;
+
 gulp.task('sass', () => {
-    return gulp.src('styles/**/*.scss').pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError)).pipe(gulp.dest('assets'));
+    return gulp.src(srcSCSS).pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError)).pipe(gulp.dest(assetsDir));
+});
+
+/**
+ * JS task
+ * Note: use npm to install libraries and add them below, like the babel-polyfill example
+ */
+ const jsFiles = [
+    //`./node_modules/babel-polyfill/dist/polyfill.js`,
+    srcJS
+];
+
+gulp.task('fonts', () => {
+    return gulp.src('fonts/**')
+        .pipe(changed(assetsDir)) // ignore unchanged files
+        .pipe(gulp.dest(assetsDir))
 });
 
 gulp.task('watch', () => {
-    gulp.watch('styles/**/*.scss', gulp.series('sass'));
+    gulp.watch(srcSCSS, gulp.series('sass'));
 });
