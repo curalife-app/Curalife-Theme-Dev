c470c6418629c0bbc9ebde245b755717140a505a
Yotam Faraggi <yotam@curalife.com>
2022-09-05 14:36:57 +0300

    Mon Sep  5 14:36:57 IDT 2022

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -2,7 +2,7 @@ const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 
 gulp.task('sass', () => {
-    return gulp.src('styles/*.scss').pipe(sass({outputStyle: 'compressed'})).pipe(gulp.dest('assets'));
+    return gulp.src('styles/**/*.scss').pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError)).pipe(gulp.dest('assets'));
 });
 
 gulp.task('watch', () => {
