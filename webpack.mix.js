4b58b02f0fdd812d00f053b850a81cbfdf96f76f
Yotam Faraggi <yotam@curalife.com>
2023-02-20 15:19:48 +0200

    Mon Feb 20 15:19:48 IST 2023

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,7 @@
 let mix = require('laravel-mix');
 const tailwindcss = require('tailwindcss');
 
-mix.js('src/scripts/*.js', 'dist/assets').setPublicPath('dist/assets')
+mix.js('src/scripts/*.js', 'dist/assets')
   .options({
     processCssUrls: false,
     postCss: [tailwindcss('tailwind.config.js')],
