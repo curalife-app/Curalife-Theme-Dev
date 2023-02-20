a79c5d6f89fb133d43f697e942c96370f159c025
Yotam Faraggi <yotam@curalife.com>
2023-02-20 18:15:35 +0200

    Mon Feb 20 18:15:35 IST 2023

--- webpack.mix.js
+++ webpack.mix.js
@@ -16,34 +16,32 @@ const paths = {
   build_layout_folder: 'build/layout/',
   build_sections_folder: 'build/sections/',
   build_snippets_folder: 'build/snippets/',
-  styles: 'src/styles/',
   css_folder: 'src/styles/css/',
   scss_folder: 'src/styles/scss/',
-  scripts: 'src/scripts/**/*.js',
-  images: 'src/images/*/*.{png,jpg,jpeg,gif,svg}',
-  fonts: 'src/fonts/**/*.{woff,woff2,eot,ttf}',
-  layout_folder: 'src/liquid/layout/',
-  config_folder: 'src/liquid/config/',
-  locales_folder: 'src/liquid/locales/',
-  sections_folder: 'src/liquid/sections/',
-  snippets_folder: 'src/liquid/snippets/',
+  script_files: 'src/scripts/**/*.js',
+  image_files: 'src/images/*/*.{png,jpg,jpeg,gif,svg}',
+  font_files: 'src/fonts/**/*.{woff,woff2,eot,ttf}',
+  layout_folder_files: 'src/liquid/layout/**',
+  config_folder_files: 'src/liquid/config/**',
+  locales_folder_files: 'src/liquid/locales/**',
+  sections_folder_files: 'src/liquid/sections/**',
+  snippets_folder_files: 'src/liquid/snippets/**',
   templates_folder: 'src/liquid/templates/'
 }
 
 mix.clean({cleanOnceBeforeBuildPatterns: [paths.build_folder]});
 
-mix.js(paths.scripts, paths.build_assets_folder)
-  .copy(paths.fonts, paths.build_assets_folder)
-  .copy(paths.images, paths.build_assets_folder)
-  .copy(paths.layout_folder, paths.build_layout_folder)
-  .copy(paths.sections_folder, paths.build_sections_folder)
-  .copy(paths.snippets_folder, paths.build_snippets_folder)
+mix.js(paths.script_files, paths.build_assets_folder)
+  .copy(paths.font_files, paths.build_assets_folder)
+  .copy(paths.image_files, paths.build_assets_folder)
+  .copy(paths.layout_folder_files, paths.build_layout_folder)
+  .copy(paths.sections_folder_files, paths.build_sections_folder)
+  .copy(paths.snippets_folder_files, paths.build_snippets_folder)
   .copy(paths.templates_folder, paths.build_templates_folder, false)
   .options({
     processCssUrls: false,
     postCss: [tailwindcss('tailwind.config.js')],
   });
-
 
   getFiles(paths.scss_folder).forEach(function (filepath) {
       mix.sass(paths.scss_folder + filepath, paths.build_assets_folder);
