d4af31be0a09408410da288904d8902d377f90d1
Yotam Faraggi <yotam@curalife.com>
2023-03-14 19:59:10 +0200

    .

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,48 +1,54 @@
-let mix = require('laravel-mix');
+const mix = require('laravel-mix');
 const tailwindcss = require('tailwindcss');
-let fs = require('fs');
+const fs = require('fs');
 
-let getFiles = function (dir) {
-  return fs.readdirSync(dir).filter(file => {
-      return fs.statSync(`${dir}/${file}`).isFile();
-  });
+const getFiles = (dir) => {
+  return fs.readdirSync(dir).filter(file => fs.statSync(`${dir}/${file}`).isFile());
 };
 
 const paths = {
-  build_folder: 'Curalife-Theme-Build/',
-  build_assets_folder: 'Curalife-Theme-Build/assets/',
-  build_templates_folder: 'Curalife-Theme-Build/templates/',
-  build_layout_folder: 'Curalife-Theme-Build/layout/',
-  build_sections_folder: 'Curalife-Theme-Build/sections/',
-  build_snippets_folder: 'Curalife-Theme-Build/snippets/',
-  build_locales_folder: 'Curalife-Theme-Build/locales/',
+  build_folder: 'Curalife-Theme-Build',
+  build_assets_folder: 'Curalife-Theme-Build/assets',
+  build_templates_folder: 'Curalife-Theme-Build/templates',
+  build_layout_folder: 'Curalife-Theme-Build/layout',
+  build_sections_folder: 'Curalife-Theme-Build/sections',
+  build_snippets_folder: 'Curalife-Theme-Build/snippets',
   css_folder_files: 'src/styles/css/**',
+  font_files: 'src/fonts/**/*.{woff,woff2,eot,ttf}',
+  image_files: 'src/images/*/*.{png,jpg,jpeg,gif,svg}',
+  layout_folder_files: 'src/liquid/layout/**',
   scss_folder: 'src/styles/scss/',
   script_files: 'src/scripts/**/*.js',
-  image_files: 'src/images/*/*.{png,jpg,jpeg,gif,svg}',
-  font_files: 'src/fonts/**/*.{woff,woff2,eot,ttf}',
-  layout_folder_files: 'src/liquid/layout/**',
-  locales_folder_files: 'src/locales/**',
   sections_folder_files: 'src/liquid/sections/**',
   snippets_folder_files: 'src/liquid/snippets/**',
   tailwindcss_file: 'src/styles/tailwind.scss'
 }
 
-// Example for not flattening folder: .copy(paths.templates_folder, paths.build_templates_folder, false)
+// Copy all required files to target destination
+mix.copy(paths.script_files, paths.build_assets_folder);
+mix.copy(paths.font_files, paths.build_assets_folder);
+mix.copy(paths.css_folder_files, paths.build_assets_folder);
+mix.copy(paths.image_files, paths.build_assets_folder);
+mix.copy(paths.layout_folder_files, paths.build_layout_folder);
+mix.copy(paths.sections_folder_files, paths.build_sections_folder);
+mix.copy(paths.snippets_folder_files, paths.build_snippets_folder);
 
-mix.copy(paths.script_files, paths.build_assets_folder)
-    .copy(paths.font_files, paths.build_assets_folder)
-    .copy(paths.css_folder_files, paths.build_assets_folder)
-    .copy(paths.image_files, paths.build_assets_folder)
-    .copy(paths.layout_folder_files, paths.build_layout_folder)
-    .copy(paths.sections_folder_files, paths.build_sections_folder)
-    .copy(paths.snippets_folder_files, paths.build_snippets_folder)
-    .sass(paths.tailwindcss_file, paths.build_assets_folder)
-      .options({
-        processCssUrls: false,
-        postCss: [tailwindcss('tailwind.config.js')],
-      });
+// Compile all SCSS source files using TailwindCSS
+mix.sass(paths.tailwindcss_file, paths.build_assets_folder)
+   .options({
+    processCssUrls: false,
+    postCss: [ tailwindcss('tailwind.config.js') ],
+   });
 
-  getFiles(paths.scss_folder).forEach(function (filepath) {
-      mix.sass(paths.scss_folder + filepath, paths.build_assets_folder);
-  });
+// Compile each individual SCSS into CSS
+getFiles(paths.scss_folder).forEach(filename =>
+  mix.sass(`${paths.scss_folder}${filename}`, paths.build_assets_folder));
+
+  // mix.browserSync({
+  //     proxy: 'http://localhost:3000',
+  //     files: [
+  //         // The path(s) to your templates or Liquid files that will trigger a rebuild when changed
+  //       paths.sections_folder_files,
+  //       paths.snippets_folder_files
+  //     ]
+  // });
