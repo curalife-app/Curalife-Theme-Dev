cd9d82d06a5c774fab4ce691318b18321d7dc946
yotam-curalife <yotam@curalife.com>
2024-01-04 16:26:22 +0200

    .

--- assets/product-form.js
+++ assets/product-form.js
@@ -92,6 +92,7 @@ if (!customElements.get('product-form')) {
             if (this.cart && this.cart.classList.contains('is-empty')) this.cart.classList.remove('is-empty');
             if (!this.error) this.submitButton.removeAttribute('aria-disabled');
             this.querySelector('.loading-overlay__spinner').classList.add('hidden');
+            this.form.querySelector('[name=quantity]').value = 1;
           });
       }
 
