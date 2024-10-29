2c00a0b573209b8079c43701577ae3358f80c1cc
yotam-curalife <yotam@curalife.com>
2024-10-29 17:18:43 +0200

    .

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,10 +13,6 @@ function copyFiles(source, destination, isFlattened = false) {
   mix.copy(source, destination, { flatten: isFlattened });
 }
 
-function processFiles(source, destination) {
-  mix.js(source, destination);
-}
-
 const paths = {
   build_folder: 'Curalife-Theme-Build',
   build_assets_folder: 'Curalife-Theme-Build/assets',
@@ -29,13 +25,14 @@ const paths = {
   font_files: 'src/fonts/**/*.{woff,woff2,eot,ttf,otf}',
   image_files: 'src/images/**/*.{png,jpg,jpeg,gif,svg}',
   layout_folder_files: 'src/liquid/layout/**',
-  scss_folder: 'src/styles/scss/',
+  // Remove SCSS folder path since we're not using SCSS anymore
+  // scss_folder: 'src/styles/scss/',
   script_files: 'src/scripts/**/*.js',
   sections_folder_files: 'src/liquid/sections/**/*.liquid',
   snippets_folder_files: 'src/liquid/snippets/**/*.liquid',
   blocks_folder_files: 'src/liquid/blocks/**/*.liquid',
-  tailwindcss_file: 'src/styles/tailwind.scss'
-}
+  tailwindcss_file: 'src/styles/tailwind.css'
+};
 
 console.log("Configuring Webpack...");
 
@@ -52,6 +49,12 @@ mix.webpackConfig({
         ]
       }
     ]
+  },
+  stats: {
+    all: undefined,
+    warnings: true,
+    errors: true,
+    children: true,
   }
 });
 
@@ -64,29 +67,21 @@ copyFiles(paths.font_files, paths.build_assets_folder, false);
 copyFiles(paths.css_folder_files, paths.build_assets_folder, false);
 copyFiles(paths.image_files, paths.build_assets_folder, true);
 
-// copy liquid files
+// Copy liquid files
 copyFiles(paths.layout_folder_files, paths.build_layout_folder);
 copyFiles(paths.sections_folder_files, paths.build_sections_folder);
 copyFiles(paths.snippets_folder_files, paths.build_snippets_folder);
 copyFiles(paths.blocks_folder_files, paths.build_blocks_folder);
 
-// processFiles('src/liquid/sections/pages/blogs/articles/article.liquid', '/')
-
 console.log("File copying configured.");
-console.log("Configuring SCSS compilation...");
+console.log("Configuring Tailwind CSS compilation...");
 
-// Compile all SCSS source files using TailwindCSS
-mix.sass(paths.tailwindcss_file, paths.build_assets_folder)
-  .options({
-    processCssUrls: false,
-    postCss: [tailwindcss('tailwind.config.js')],
-  });
+// Compile Tailwind CSS using PostCSS
+mix.postCss(paths.tailwindcss_file, paths.build_assets_folder, [
+  tailwindcss('tailwind.config.js'),
+]).options({
+  processCssUrls: false,
+});
 
-console.log("SCSS compilation configured.");
-console.log("Compiling individual SCSS files...");
-
-// Compile each individual SCSS into CSS
-getFiles(paths.scss_folder).forEach(filename =>
-  mix.sass(`${paths.scss_folder}${filename}`, paths.build_assets_folder));
-
+console.log("Tailwind CSS compilation configured.");
 console.log("Laravel Mix configuration complete.");
