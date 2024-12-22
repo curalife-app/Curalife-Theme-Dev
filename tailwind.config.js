36c01873694fa7c16dcd0305c7b8b82585cf134e
yotam-curalife <yotam@curalife.com>
2024-12-22 10:42:06 +0200

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -42,5 +42,8 @@ module.exports = {
 	variants: {
 		textColor: ["odd", "even"]
 	},
-	plugins: []
+	modules: {
+		display: ["responsive", "empty"]
+	},
+	plugins: [require("tailwindcss-empty")()]
 };
