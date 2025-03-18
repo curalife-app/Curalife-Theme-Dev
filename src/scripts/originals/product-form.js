9e2ed34e62a2ac84c8ba8204d5d676b0f929300f
yotam-curalife <yotam@curalife.com>
2025-03-18 15:22:07 +0100

    .

--- src/scripts/originals/product-form.js
+++ src/scripts/originals/product-form.js
@@ -126,3 +126,102 @@ if (!customElements.get("product-form")) {
 		}
 	);
 }
+
+const ON_CHANGE_DEBOUNCE_TIMER = 300;
+
+const PUB_SUB_EVENTS = {
+	cartUpdate: "cart-update",
+	quantityUpdate: "quantity-update",
+	variantChange: "variant-change"
+};
+
+let subscribers = {};
+
+function subscribe(eventName, callback) {
+	if (subscribers[eventName] === undefined) {
+		subscribers[eventName] = [];
+	}
+
+	subscribers[eventName] = [...subscribers[eventName], callback];
+
+	return function unsubscribe() {
+		subscribers[eventName] = subscribers[eventName].filter(cb => {
+			return cb !== callback;
+		});
+	};
+}
+
+function publish(eventName, data) {
+	if (subscribers[eventName]) {
+		subscribers[eventName].forEach(callback => {
+			callback(data);
+		});
+	}
+}
+
+class QuantityInput extends HTMLElement {
+	constructor() {
+		super();
+		this.input = this.querySelector("input");
+		this.changeEvent = new Event("change", { bubbles: true });
+
+		this.input.addEventListener("change", this.onInputChange.bind(this));
+		this.querySelectorAll("button").forEach(button => button.addEventListener("click", this.onButtonClick.bind(this)));
+	}
+
+	quantityUpdateUnsubscriber = undefined;
+
+	connectedCallback() {
+		this.validateQtyRules();
+		this.quantityUpdateUnsubscriber = subscribe(PUB_SUB_EVENTS.quantityUpdate, this.validateQtyRules.bind(this));
+	}
+
+	disconnectedCallback() {
+		if (this.quantityUpdateUnsubscriber) {
+			this.quantityUpdateUnsubscriber();
+		}
+	}
+
+	onInputChange(event) {
+		this.validateQtyRules();
+	}
+
+	onButtonClick(event) {
+		event.preventDefault();
+		const previousValue = this.input.value;
+
+		event.target.name === "plus" ? this.input.stepUp() : this.input.stepDown();
+		if (previousValue !== this.input.value) this.input.dispatchEvent(this.changeEvent);
+	}
+
+	validateQtyRules() {
+		const value = parseInt(this.input.value);
+		if (this.input.min) {
+			const min = parseInt(this.input.min);
+			const buttonMinus = this.querySelector(".quantity__button[name='minus']");
+			buttonMinus.classList.toggle("disabled", value <= min);
+		}
+		if (this.input.max) {
+			const max = parseInt(this.input.max);
+			const buttonPlus = this.querySelector(".quantity__button[name='plus']");
+			buttonPlus.classList.toggle("disabled", value >= max);
+		}
+	}
+}
+
+customElements.define("quantity-input", QuantityInput);
+
+function debounce(fn, wait) {
+	let t;
+	return (...args) => {
+		clearTimeout(t);
+		t = setTimeout(() => fn.apply(this, args), wait);
+	};
+}
+
+function fetchConfig(type = "json") {
+	return {
+		method: "POST",
+		headers: { "Content-Type": "application/json", Accept: `application/${type}` }
+	};
+}
