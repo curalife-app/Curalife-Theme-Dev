3fb6a8150b7ce2c6e1b17a70f34fd4b66ac06193
yotam-curalife <yotam@curalife.com>
2025-06-10 21:18:09 +0200

    .

--- gcp/cloud-functions/workflow_user_creation/workflow-client.js
+++
@@ -1,6 +0,0 @@
-const {ExecutionsClient} = require('@google-cloud/workflows');
-
-// Initialize client that will be used to create and send requests.
-const workflowsClient = new ExecutionsClient();
-
-module.exports = { workflowsClient };
